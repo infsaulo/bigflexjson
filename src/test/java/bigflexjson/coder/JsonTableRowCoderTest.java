a82f396e6560b7dacc930da089d03e149b780880
Saulo Ricci <infsaulok@gmail.com>
2016-07-18 20:53:55 -0300

    Adding JsonTableRowCoderTest.java

---
+++ src/test/java/bigflexjson/coder/JsonTableRowCoderTest.java
@@ -0,0 +1,46 @@
+package bigflexjson.coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.beam.sdk.coders.Coder.Context;
+import org.apache.beam.sdk.coders.CoderException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.google.api.services.bigquery.model.TableRow;
+
+import bigflexjson.grammar.Grammar;
+import bigflexjson.grammar.GrammarParser;
+
+public class JsonTableRowCoderTest {
+
+  @Mock
+  final Context context = new Context(true);
+
+  @Test
+  public void testJsonTableRowCoder() throws CoderException, IOException {
+
+    final String grammarRepr = "{\"fields\":["
+        + "{\"name\":\"field1\",\"srcType\":\"INTEGER\",\"bqType\":\"STRING\", \"destName\":\"field_1\"},"
+        + "{\"name\":\"field2\",\"srcType\":\"STRING\",\"bqType\":\"STRING\", \"destName\":\"field_2\"}"
+        + "]}";
+
+    final Grammar grammar = GrammarParser.getGrammar(grammarRepr);
+
+    final String jsonObjStr = "{\"field1\":42, \"field2\": \"abgef102\"}";
+    final InputStream jsonObjInputStream =
+        new ByteArrayInputStream(jsonObjStr.getBytes(StandardCharsets.UTF_8));
+
+    final JsonTableRowCoder coder = new JsonTableRowCoder(grammar);
+
+    final TableRow row = coder.decode(jsonObjInputStream, context);
+
+    Assert.assertTrue(((String) row.get("field_1")).equals("42"));
+    Assert.assertTrue(((String) row.get("field_2")).equals("abgef102"));
+  }
+
+}
