9aadc381dad4bfa780b8d861dbc535982d31cd85
Saulo Ricci <infsaulok@gmail.com>
2016-07-27 21:14:26 -0300

    Getting rid of warnings

--- src/test/java/bigflexjson/grammar/GrammarParseTest.java
+++ src/test/java/bigflexjson/grammar/GrammarParseTest.java
@@ -74,7 +74,8 @@ public class GrammarParseTest {
         + "{\"name\":\"field1\",\"srcType\":\"INTEGER\",\"bqType\":\"INTEGER\", \"destName\":\"field_1\"},"
         + "{\"name\":\"field2\",\"srcType\":\"STRING\",\"bqType\":\"STRING\", \"destName\":\"field_2\", "
         + "\"srcSerialization\":\"hex\"},"
-        + "{\"name\":\"field3\", \"srcType\":\"RECORD\",\"bqType\":\"RECORD\", \"destName\":\"field_3\", \"fields\": [{\"name\":\"innerfield1\",\"srcType\":\"INTEGER\",\"bqType\":\"INTEGER\", \"destName\":\"inner_field_1\"},"
+        + "{\"name\":\"field3\", \"srcType\":\"RECORD\",\"bqType\":\"RECORD\", \"destName\":\"field_3\", \"fields\": "
+        + "[{\"name\":\"innerfield1\",\"srcType\":\"INTEGER\",\"bqType\":\"INTEGER\", \"destName\":\"inner_field_1\"},"
         + "{\"name\":\"innerfield2\",\"srcType\":\"STRING\",\"bqType\":\"STRING\", \"destName\":\"inner_field_2\", "
         + "\"srcSerialization\":\"hex\"}]}]}";
 
